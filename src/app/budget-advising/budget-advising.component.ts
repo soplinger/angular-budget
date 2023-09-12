41697546a45e7481fc2a9ca7d70fec628102b075
Sean Oplinger <sean@soplinger.com>
2023-09-11 23:22:00 -0400

    more updates

--- src/app/budget-advising/budget-advising.component.ts
+++ src/app/budget-advising/budget-advising.component.ts
@@ -12,10 +12,9 @@ export class BudgetAdvisingComponent {
   financialGoal: any;
   formData: any;
   showQuestion: boolean = true;
-  debtAdvice: string = '';
   userTotalCash: number = 0;
   userEmergencyFund: number = 0;
-
+  public debtAdvice: string[] = [];
 
   financialGoalOptions: string[] = ['Pay off Debt', 'Save for the Future', 'Save for a Large Purchase', 'Live Within My Means'];
 
@@ -49,7 +48,7 @@ export class BudgetAdvisingComponent {
 
   handleDebt() {
     console.log('handleDebt');
-  
+    this.debtAdvice = [];
     const income = this.formData.postTaxIncome;
   
     // Calculate total expenses
@@ -72,12 +71,12 @@ export class BudgetAdvisingComponent {
     this.userTotalCash = totalCash;
     this.userEmergencyFund = availableCash > 0 ? emergencyFundRequired : totalCash;
   
-    this.debtAdvice = `Based on your monthly necessities, you should aim for an emergency fund of $${emergencyFundRequired.toFixed(2)}. `;
+    this.debtAdvice.push(`Based on your monthly necessities, you should aim for an emergency fund of $${emergencyFundRequired.toFixed(2)}. `);
   
     if (totalCash < emergencyFundRequired) {
-      this.debtAdvice += `You currently have $${totalCash.toFixed(2)}, which means you need an additional $${(emergencyFundRequired - totalCash).toFixed(2)} to reach your emergency fund goal. Consider building this fund before aggressively paying off debt. `;
+      this.debtAdvice.push(`You currently have $${totalCash.toFixed(2)}, which means you need an additional $${(emergencyFundRequired - totalCash).toFixed(2)} to reach your emergency fund goal. Consider building this fund before aggressively paying off debt. `);
     } else {
-      this.debtAdvice += `You have an excess of $${availableCash.toFixed(2)} after setting aside a 6-month emergency fund. Consider using this amount to pay off your debt faster. `;
+      this.debtAdvice.push(`You have an excess of $${availableCash.toFixed(2)} after setting aside a 6-month emergency fund. Consider using this amount to pay off your debt faster. `);
     }
   
     // Sort credit cards based on interest rate and provide advice
@@ -88,12 +87,12 @@ export class BudgetAdvisingComponent {
     if (sortedCreditCards.length > 0) {
       sortedCreditCards.forEach((card: { nickname: string, interestRate: number, amount: number }) => {
         const monthsToPayOff = card.amount / (income - expenses);
-        this.debtAdvice += `Pay off the credit card nicknamed "${card.nickname}" with an interest rate of ${card.interestRate}%. It will take approximately ${Math.ceil(monthsToPayOff)} months to pay off. `;
+        this.debtAdvice.push(`Pay off the credit card nicknamed "${card.nickname}" with an interest rate of ${card.interestRate}%. It will take approximately ${Math.ceil(monthsToPayOff)} months to pay off. `);
       });
     }
   
     // Additional advice: Importance of budgeting and monitoring expenses
-    this.debtAdvice += `Regularly monitor your expenses and adjust your budget as needed. This will help you stay on track with your financial goals and avoid accumulating more debt.`;
+    this.debtAdvice.push(`Regularly monitor your expenses and adjust your budget as needed. This will help you stay on track with your financial goals and avoid accumulating more debt.`);
   
     console.log(this.debtAdvice);
   }
